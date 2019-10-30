fb245fd4ed63b119c59dc0cb145fb8111819ca9a
Dominic DiFranzo <dominic@difranzo.com>
2019-10-30 18:18:44 -0400

    Update main.js

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,5 @@
 /*
- * This files holds all the code to test you REST API
+ * This files holds all the code to for your card game
  */
 
 //Run once broswer has loaded everything
@@ -7,28 +7,6 @@ window.onload = function () {
 
 
 
-//button event for create
-document.getElementById("create")
-.addEventListener("click",function(e){
-    console.log("create")
-},false);
-
-//button event for read
-document.getElementById("read")
-.addEventListener("click",function(e){
-    console.log("read")
-},false);
-
-//button event for update
-document.getElementById("update")
-.addEventListener("click",function(e){
-    console.log("update")
-},false);
-
-//button event for destroy
-document.getElementById("destroy")
-.addEventListener("click",function(e){
-    console.log("destroy")
-},false);
+//All your Front end code should be here!
 
 };
